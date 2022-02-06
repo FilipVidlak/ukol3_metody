aed2eeaea0bc1de635cdf9bf78b3f488dc527032
FilipVidlak <72192554+FilipVidlak@users.noreply.github.com>
2022-02-06 18:55:41 +0100

    Add files via upload

---
+++ ukol3_metody/ukol3_metody/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace ukol3_metody
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Nova Cond", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(603, 263);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(553, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Je alfanum?";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(231, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 93);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Proveď funkce";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Nova", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(183, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(278, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Zadej libovolný počet znaků:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(725, 408);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ukol3_metody/ukol3_metody/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ukol3_metody
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void JeAlfaNum()
+        {
+            int countnotalfanum = 0;
+            foreach (char lol in textBox1.Text)
+            {
+                if (Char.IsLetterOrDigit(lol)) {
+                    label1.Text = "Ano, je!";
+                }
+                else { label1.Text = "Ne, není!"; countnotalfanum++; }
+            }
+            MessageBox.Show("Obsahuje " + countnotalfanum + " jiných znaků", "NOT ALFANUM", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void MalaPismena()
+        {
+            int lowerchar = 0;
+            foreach (char lol in textBox1.Text) {
+                if (Char.IsLower(lol)) { lowerchar++; }
+            }
+            MessageBox.Show("Obsahuje " + lowerchar + " malých písmen", "LOWERCASE LETTER", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        public void VelkaPismena()
+        {
+            int upperchar = 0;
+            foreach (char lol in textBox1.Text) {
+                if (Char.IsUpper(lol)) { upperchar++; }
+            }
+            MessageBox.Show("Obsahuje " + upperchar + " velkých písmen", "UPPERCASE LETTER", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+            private void button1_Click(object sender, EventArgs e)
+            {
+             JeAlfaNum();
+             MalaPismena();
+             VelkaPismena();
+            textBox1.Clear();
+            label1.Text = "---";
+            }
+    }
+}
